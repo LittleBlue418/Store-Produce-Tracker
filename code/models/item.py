67e17ff395b3bdca42f8e31eeb7eae157c5760a8
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-20 14:06:08 +0100

    building out & refactoring Item Model

---
+++ code/models/item.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+class ItemModel:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def json(self):
+        return {'name': self.name, 'price': self.price}
+
+
+    # Moving code that we re-use out into a class method,
+    # this is used by both get & post
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return cls(*row)
+
+
+    def insert(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (self.name, self.price))
+
+        connection.commit()
+        connection.close()
+
+
+    def update(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        # Without the WHERE name=? it would update ALL prices!
+        query = "UPDATE items SET price=? WHERE name=?"
+        # Arguments in the tuple must be in the correct order
+        # Whichever question mark is first, that argument first etc
+        cursor.execute(query, (self.price, self.name))
+
+        connection.commit()
+        connection.close()
