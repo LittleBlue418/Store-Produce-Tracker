04dfe3fadc6db6002075acf3c7e27c018a46dbf4
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-20 14:26:09 +0100

    refactoring after creating Item Model class

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+from models.item import ItemModel
 
 
 # Our items class, with the different end points as methods. Note
@@ -19,57 +20,29 @@ class Item(Resource):
     # must be authenticated and have an auth key to do anything with it.
     @jwt_required()
     def get(self, name):
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {'message': 'Item not found'}
-
-
-    # Moving code that we re-use out into a class method, this is used by both get & post
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'item': {'name': row[0], 'price': row[1]}}
 
 
     def post(self, name):
         # 'error first' - we only run the rest of the code if there are no errors.
         #  This helps us move faster, we are not loaidng things we don't need.
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
         request_data = Item.parser.parse_args()
 
-        item = {'name': name, 'price': request_data['price']}
+        item = ItemModel(name, request_data['price'])
 
         # To capture if there has been an error posting for whatever reason
         try:
-            self.insert(item)
+            item.insert()
         except:
             return {"message": "An error occurred"}, 500
 
         return item, 201
-
-
-    # refactoring code shared by POST and PUT
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
 
 
 
@@ -90,36 +63,22 @@ class Item(Resource):
     def put(self, name):
         request_data = Item.parser.parse_args()
 
-        item = self.find_by_name(name)
-        updated_item = {'name': name, 'price': request_data['price']}
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, request_data['price'])
 
         # Using try / except will give our users nice friendly error messages
         if item is None:
             try:
-                self.insert(updated_item)
+                updated_item.insert()
             except:
                 return {"message": "An error occured inserting the item"}, 500 # always use the correct code!
         else:
             try:
-                self.update(updated_item)
+                updated_item.update()
             except:
                 return {"message": "An error occured updating the item"}, 500
-        return updated_item
+        return updated_item.json()
 
-
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        # Without the WHERE name=? it would update ALL prices!
-        query = "UPDATE items SET price=? WHERE name=?"
-        # Arguments in the tuple must be in the correct order
-        # Whichever question mark is first, that argument first etc
-        cursor.execute(query, (item['price'], item['name']))
-
-        connection.commit()
-        connection.close()
 
 
 # A seporate class with a seporate end point to get all of the items.
