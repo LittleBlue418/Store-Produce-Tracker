254b4cfa7ed390836c3a228ef2169b1e51c82941
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-24 12:18:22 +0100

    extending models with SQLAlchemy table set-up

--- code/models/user.py
+++ code/models/user.py
@@ -1,8 +1,16 @@
 import sqlite3
+from db import db
 
 # Creating our user class to help us store and access
 # data in a better way
-class UserModel:
+class UserModel(db.Model):
+    # Setting up SQLAlchemy tables
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
