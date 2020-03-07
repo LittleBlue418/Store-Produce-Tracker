37a8960c20659088e9004123069aa64090b75c42
LittleBlue418 <hollyathomas88@gmail.com>
2020-03-07 10:17:44 +0100

    updating to jwt extended

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
 
@@ -22,7 +22,7 @@ class Item(Resource):
 
     # This method has the JavaScript Web Token required decorator, the user
     # must be authenticated and have an auth key to do anything with it.
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
