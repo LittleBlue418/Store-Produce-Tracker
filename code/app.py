1c77043f908663b309a926f234a28e8d3b0c3da2
LittleBlue418 <hollyathomas88@gmail.com>
2020-03-09 14:12:37 +0100

    bug fix: importing jsonify

--- code/app.py
+++ code/app.py
@@ -1,7 +1,7 @@
 import os
 
 # Importing from flask and flask libraries
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
